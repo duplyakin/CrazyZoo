8f7a4e53daf9c56c6b54d8aaeb6ad7b8334e376b
Vlad Duplyakin <DuplyakinV@gmail.com>
2018-08-14 20:46:48 +0300

    attempt to make pixijs example work

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,'./html/pixitest.html'));
+});
+
+app.use(express.static('res'));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
